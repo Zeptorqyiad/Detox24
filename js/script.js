6e10ddcb8f662d41defd4504ee6016f2efa36ad2
Max Unanian <Zeptorqyiad99@gmail.com>
2024-10-09 02:02:51 +0500

    Work with faq section

--- js/script.js
+++ js/script.js
@@ -100,3 +100,23 @@ document.addEventListener('keydown', function (e) {
          Element.prototype.msMatchesSelector
    }
 })()
+
+// Faq
+
+var details = document.querySelectorAll('details')
+for (i = 0; i < details.length; i++) {
+   details[i].addEventListener('toggle', accordion)
+}
+function accordion(event) {
+   if (!event.target.open) return
+   var details = event.target.parentNode.children
+   for (i = 0; i < details.length; i++) {
+      if (
+         details[i].tagName != 'DETAILS' ||
+         !details[i].hasAttribute('open') ||
+         event.target == details[i]
+      )
+         continue
+      details[i].removeAttribute('open')
+   }
+}
