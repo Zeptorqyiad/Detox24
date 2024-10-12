595484a1ac7ba0bd444a648f5c43493579e5b6b4
Max Unanian <Zeptorqyiad99@gmail.com>
2024-10-12 21:59:13 +0500

    Added new version of PHPMailer

--- sendmail.php
+++ sendmail.php
@@ -3,8 +3,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 $mail->CharSet = 'UTF-8';
